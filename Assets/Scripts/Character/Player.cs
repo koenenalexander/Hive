d02612b248e96d9ad4e062b148fbd3008eaced95
Alexander Koenen <wolvie28@gmail.com>
2020-12-05 11:11:12 -0600

    Player Movement
    Inital player movement work. Player moves up down left and right based on key presses or left joystick.

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Require an InputManager Component
+[RequireComponent(typeof(InputManager))]
+[RequireComponent(typeof(Rigidbody2D))]
+/// <summary>
+/// Handles all behaviors required to facilitate Player interaction
+/// </summary>
 public class Player : MonoBehaviour
 {
+    private InputManager input;
+    private Rigidbody2D body;
+    [SerializeField]
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // We've ensured this object will have an InputManager 
+        // and a Rigidbody2d by using the 'RequireComponent' attribute
+        input = GetComponent<InputManager>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Update position
+    /// </summary>
+    /// <remarks>
+    /// Fixed Update is synchronized with the physics engine
+    /// so all application of physics forces needs to be done here
+    /// </remarks>
+    private void FixedUpdate()
+    {
+        body.position += (input.CommandedMovement * speed);
     }
 }
