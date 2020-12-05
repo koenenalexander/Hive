dcfd2c384be76f6eb57702f0ece3f26a85e2cd6a
Alexander Koenen <wolvie28@gmail.com>
2020-12-05 10:52:21 -0600

    Adding Commanded Movement
    Adding in handling of movement commands to the Input Manager script.

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Abstracts Input signals into usable commands
+/// </summary>
 public class InputManager : MonoBehaviour
 {
+    private Vector2 _commandedMovement;
+    public Vector2 CommandedMovement 
+    { 
+        get => _commandedMovement; 
+    }
+
+    // The Axis names are set based on the Unity Input.GetAxis documenation here:
+    // https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+    private const string _verticalAxis = "Vertical";
+    private const string _horizontalAxis = "Horizontal";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _commandedMovement = new Vector2(0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _commandedMovement.x = Input.GetAxis(_horizontalAxis);
+        _commandedMovement.y = Input.GetAxis(_verticalAxis);
     }
 }
+
