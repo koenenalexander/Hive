ee6eaf23a6189c3e6cbed4409f8b64ab84c29f77
Alexander Koenen <wolvie28@gmail.com>
2020-12-06 07:03:11 -0600

    Init Projectile Script
    Adding initial projectile script

---
+++ Assets/Scripts/Character/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    private float speed;
+    private Vector2 direction;
+    private Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        body.position += direction * speed;
+    }
+}
