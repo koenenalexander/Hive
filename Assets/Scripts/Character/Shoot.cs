7ed025245c5b8a910d88c0d3a5b4502d6a3602f7
Alexander Koenen <wolvie28@gmail.com>
2020-12-06 06:49:45 -0600

    Init Shoot Script
    Adding initial shoot script with a member for a projectile.

---
+++ Assets/Scripts/Character/Shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (projectile == null)
+            Debug.LogError("Shoot.cs - Start() - No projectile assigned!", projectile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
