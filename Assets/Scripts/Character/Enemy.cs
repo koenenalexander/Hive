dc273cc929b3ba6d689c06e56c787581db364db6
Alexander Koenen <wolvie28@gmail.com>
2020-12-21 09:36:10 -0600

    Enemy Search Cone
    Adding viewcone for enemies rather than a straight line for detecting the player.

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -16,6 +16,16 @@ public class Enemy : MonoBehaviour
     private Projectile projectileComponent;
     private Rigidbody2D body;
     private Movement mover;
+    [SerializeField]
+    private int viewConeSize = 90; // Degrees
+    private int searchAngleDelta = 15; // allows for control over precision of search for target
+    private int currentSearchDelta = 0;
+    private enum TARGET_SEARCH_DIRECTION
+    {
+        Clockwise,
+        CounterClockwise
+    }
+    private TARGET_SEARCH_DIRECTION searchDirection = TARGET_SEARCH_DIRECTION.Clockwise;
     private enum ENEMY_STATE
     {
         Invalid,
@@ -62,6 +72,11 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void SearchForPlayer()
+    {
+
+    }
+
     private void FireAtPlayer()
     {
         shoot.Fire(transform.right.normalized, projectileSpawner.position, projectileObject);
@@ -75,7 +90,12 @@ public class Enemy : MonoBehaviour
     {
         bool playerSpotted = false;
         var startPos = projectileSpawner.position;
-        var endPos = startPos + transform.right * sightRange;
+        var adjustmentAngle = GetSearchAdjustmentAngle();
+        Vector3 adjustedTarget = new Vector3();
+        adjustedTarget.x = transform.right.x * Mathf.Cos(Mathf.Deg2Rad * adjustmentAngle) - transform.right.y * Mathf.Sin(Mathf.Deg2Rad * adjustmentAngle);
+        adjustedTarget.y = transform.right.x * Mathf.Sin(Mathf.Deg2Rad * adjustmentAngle) + transform.right.y * Mathf.Cos(Mathf.Deg2Rad * adjustmentAngle);
+        adjustedTarget.z = transform.right.z;
+        var endPos = startPos + adjustedTarget * sightRange;
         var ray = Physics2D.Linecast(startPos, endPos, LayerMask.GetMask("Player"));
         Debug.DrawLine(startPos, endPos, Color.red, Time.deltaTime);
         if (ray.fraction > 0)
@@ -84,5 +104,28 @@ public class Enemy : MonoBehaviour
             target = ray.transform;
         }
         return playerSpotted;
+    }
+    private int GetSearchAdjustmentAngle()
+    {
+        int maxDelta = viewConeSize / 2;
+        int minDelta = (-1 * viewConeSize / 2);
+        int adjustmentAngle = currentSearchDelta;
+        if (searchDirection == TARGET_SEARCH_DIRECTION.Clockwise)
+            adjustmentAngle += searchAngleDelta;
+        else
+            adjustmentAngle -= searchAngleDelta;
+
+        if (adjustmentAngle > maxDelta)
+        {
+            adjustmentAngle = maxDelta;
+            searchDirection = TARGET_SEARCH_DIRECTION.CounterClockwise;
+        }
+        else if (adjustmentAngle < minDelta)
+        {
+            adjustmentAngle = minDelta;
+            searchDirection = TARGET_SEARCH_DIRECTION.Clockwise;
+        }
+        currentSearchDelta = adjustmentAngle;
+        return adjustmentAngle;
     }
 }
